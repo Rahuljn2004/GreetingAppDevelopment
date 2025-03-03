a51c62160a8462e045a4cde3be33765771a23259
Rahuljn2004 <rahul.jaincs21@gmail.com>
2025-03-03 22:36:03 +0530

    Welcome Page functionality

--- GreetingAppDevelopment/src/main/java/com/example/GreetingAppDevelopment/GreetingAppDevelopmentApplication.java
+++ GreetingAppDevelopment/src/main/java/com/example/GreetingAppDevelopment/GreetingAppDevelopmentApplication.java
@@ -2,12 +2,28 @@ package com.example.GreetingAppDevelopment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class GreetingAppDevelopmentApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(GreetingAppDevelopmentApplication.class, args);
+
 	}
 
+}
+
+
+@RestController
+@RequestMapping("/")
+class WelcomeApp {
+
+	@GetMapping
+	public String Welcome() {
+		return "Welcome to Gettings App";
+	}
 }
